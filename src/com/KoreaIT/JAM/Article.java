132020fd91b40ea546652b0f08ceeaaad9f72e05
CK-0512 <dhcdkrl1004@gmail.com>
2023-04-30 23:47:17 +0900

    게시물 작성시 DB에 insert

--- src/com/KoreaIT/JAM/Article.java
+++ src/com/KoreaIT/JAM/Article.java
@@ -2,11 +2,15 @@ package com.KoreaIT.JAM;
 
 public class Article {
 	int id;
+	String regDate;
+	String updateDate;
 	String title;
 	String body;
-	
-	public Article(int id, String title, String body) {
+
+	public Article(int id, String regDate, String updateDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
 	}
